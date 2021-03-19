bd62bd3f35058ca4739588c116f79206f0b19198
MeetBudheliya <Meetbudheliya04@gmail.com>
2021-03-19 12:47:09 +0530

    19/03/2021

--- CircularProgressDemo/ViewController.swift
+++ CircularProgressDemo/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
-
+import KDCircularProgress
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var progresssView: KDCircularProgress!
+    @IBOutlet weak var progressLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(progressRound), userInfo: nil, repeats: true)
+        
     }
-
+    @objc func progressRound(){
+        if progresssView.angle < 360{
+            progresssView.angle = progresssView.angle + 1
+            progressLabel.text = "\(Int((progresssView.angle*100)/360)) %"
+        }else{
+            progresssView.angle = 0
+            progressLabel.text = "\(Int((progresssView.angle*100)/360))) %"
+        }
+        
+    }
 
 }
 
